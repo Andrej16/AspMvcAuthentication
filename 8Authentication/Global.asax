abb118ffce7988a389e1e8669a8939836b9ca2f0
Andrej16 <andrej.shilin@outlook.com>
2020-07-30 14:03:17 +0300

    Clone project
    First commit

---
+++ 8Authentication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_8Authentication.MvcApplication" Language="C#" %>
---
+++ 8Authentication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_8Authentication.Startup))]
+namespace _8Authentication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
